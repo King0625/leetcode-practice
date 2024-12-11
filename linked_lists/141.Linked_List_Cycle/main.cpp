4f1fa7e760d88b0b8c49bf263014b1c4366a9961
kenchen <independentchen@gmail.com>
2024-12-11 23:56:16 +0800

    Feat: finish #141

---
+++ linked_lists/141.Linked_List_Cycle/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+bool hasCycle(ListNode *head) {
+	ListNode *slow = head;
+	ListNode *fast = head;
+
+	while(fast != nullptr && fast->next != nullptr) {
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if(fast == slow) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int main() {
+	return 0;
+}
