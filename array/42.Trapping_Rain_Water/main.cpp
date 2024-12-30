d3d68538847bfdc833b4fcc29085d2bb73f40f0b
kenchen <independentchen@gmail.com>
2024-12-30 11:43:53 +0800

    Feat: finish #42  with lookups; there are still other approaches

---
+++ array/42.Trapping_Rain_Water/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// try to use two pointers
+int trap(vector<int>& height) {
+	int left = 0;
+	int right = height.size() - 1;
+
+	int leftMax = height[left];
+	int rightMax = height[right];
+	int water = 0;
+	
+	while(left < right) {
+		if(leftMax < rightMax) {
+			left++;
+			leftMax = max(leftMax, height[left]);
+			water += leftMax - height[left];
+		} else {
+			right--;
+			rightMax = max(rightMax, height[right]);
+			water += rightMax - height[right];
+		}
+	}
+
+	return water;
+}
+
+int main(){
+	vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+
+	cout << trap(height) << "\n";
+}
