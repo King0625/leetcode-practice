0b1ddcb6d59fcdbefafcc045db3e2b07ab481ca6
kenchen <independentchen@gmail.com>
2025-01-01 11:07:11 +0800

    Refactor: foramtting and using namespace std

--- hashmap/1.Two_Sum/main.cpp
+++ hashmap/1.Two_Sum/main.cpp
@@ -2,35 +2,33 @@
 #include <map>
 #include <vector>
 
-using std::cout;
-using std::map;
-using std::vector;
+using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
-  map<int, int> num_map;
-  for (int i = 0; i < nums.size(); i++) {
-    num_map[nums[i]] = i;
-  }
+	map<int, int> num_map;
+	for (int i = 0; i < nums.size(); i++) {
+		num_map[nums[i]] = i;
+	}
 
-  vector<int> result;
-  for (int j = 0; j < nums.size(); j++) {
-    int sub = target - nums[j];
-    if (num_map.count(sub) && j != num_map[sub]) {
-      result = {num_map[sub], j};
-      break;
-    }
-  }
+	vector<int> result;
+	for (int j = 0; j < nums.size(); j++) {
+		int sub = target - nums[j];
+		if (num_map.count(sub) && j != num_map[sub]) {
+			result = {num_map[sub], j};
+			break;
+		}
+	}
 
-  return result;
+	return result;
 }
 
 int main() {
-  vector<int> nums = {3, 2, 4};
-  int target = 6;
-  vector<int> result = twoSum(nums, target);
-  for (int num : result) {
-    cout << num << " ";
-  }
-  cout << "Hi";
-  return 0;
+	vector<int> nums = {3, 2, 4};
+	int target = 6;
+	vector<int> result = twoSum(nums, target);
+	for (int num : result) {
+		cout << num << " ";
+	}
+	cout << "Hi";
+	return 0;
 }
