495143521bfe9dcb3949dd20cb155884eaa1308e
kenchen <independentchen@gmail.com>
2025-01-04 14:47:21 +0800

    Fix: use string instead of char vector and improve complexity

--- array/6.Zigzag_Conversion/main.cpp
+++ array/6.Zigzag_Conversion/main.cpp
@@ -5,23 +5,22 @@ using namespace std;
 
 string convert(string s, int numRows) {
 	if(numRows == 1) return s;
-	vector<vector<char>> converted(numRows);
-	
-	//4 + (4 - 2) + 4 + (4 - 2)
+	vector<string> converted(numRows, "");
+
 	int position = 0;
 	bool reversed = false;
 
-	for(int i = 0; i < s.size(); i++) {
-		converted[position].push_back(s[i]);
-		if(!reversed) {
-			if(position < numRows - 1){
+	for (int i = 0; i < s.size(); i++) {
+		converted[position] += s[i];
+		if (!reversed) {
+			if (position < numRows - 1) {
 				position++;
 			} else {
 				position--;
 				reversed = true;
 			}
 		} else if (reversed) {
-			if(position >= 1) {
+			if (position >= 1) {
 				position--;
 			} else {
 				position++;
@@ -31,10 +30,8 @@ string convert(string s, int numRows) {
 	}
 
 	string result = "";
-	for(int j = 0; j < converted.size(); j++) {
-		for(char c: converted[j]) {
-			result += c;
-		}
+	for (int j = 0; j < converted.size(); j++) {
+		result += converted[j];
 	}
 
 	return result;
