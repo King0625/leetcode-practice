db54b6675eeb3d9b30d2e72473723fc6940cff9d
kenchen <independentchen@gmail.com>
2025-01-07 16:15:56 +0800

    Feat: finish #236 with lookups

---
+++ binary_tree/236.Lowest_Common_Ancestor_of_a_Binary_Tree/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+	if(!root) return nullptr;
+
+	if(root == p || root == q) return root;
+
+	TreeNode* left = lowestCommonAncestor(root->left, p, q);
+	TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+	if(left && right) {
+		return root;
+	}
+
+	return left ? left : right;
+}
+
+
+
+int main(){
+	return 0;
+}
