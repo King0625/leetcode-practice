c2fafd326ffdbe1b791ae9b62e90884993083b87
kenchen <independentchen@gmail.com>
2025-01-09 22:19:20 +0800

    Feat: finish #2185 with terrible complexity

---
+++ string/2185.Counting_Words_With_a_Given_Prefix/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int prefixCount(vector<string>& words, string pref) {
+	int count = 0;
+	int prefLen = pref.size();
+	for(string s: words) {
+		int wordLen = s.size();
+		if(wordLen >= prefLen && s.substr(0, prefLen) == pref) {
+			count++;
+		} 
+	}
+
+	return count;
+}
+
+int main(){
+	vector<string> words = {"pay","attention","practice","attend"};
+	string pref = "at";
+	cout << prefixCount(words, pref);
+}
