a6d34409e8283800ac6e3b3314e27b47c80ab021
kenchen <independentchen@gmail.com>
2025-01-24 20:53:23 +0800

    Fix: change the logic in for loop to improve time complexity

--- string/2185.Counting_Words_With_a_Given_Prefix/main.cpp
+++ string/2185.Counting_Words_With_a_Given_Prefix/main.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int prefixCount(vector<string>& words, string pref) {
 	int count = 0;
 	int prefLen = pref.size();
-	for(string s: words) {
+	for (string s : words) {
 		int wordLen = s.size();
-		if(wordLen >= prefLen && s.substr(0, prefLen) == pref) {
+		if(wordLen < prefLen) continue;
+
+		if (s.substr(0, prefLen) == pref) {
 			count++;
-		} 
+		}
 	}
 
 	return count;
