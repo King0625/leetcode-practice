687af6ff2196699a79a0a9ce11ece8917039ae1e
kenchen <independentchen@gmail.com>
2025-06-02 19:14:45 +0800

    Feat: finish #0102 nicely

---
+++ golang/0101-0200/0102.BinaryTreeLevelOrderTraversal/solution.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func LevelOrder(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+
+	q := make([]*TreeNode, 0)
+	q = append(q, root)
+
+	for len(q) != 0 {
+		levelSlice := make([]int, 0)
+		qLen := len(q)
+		for i := 0; i < qLen; i++ {
+			node := q[0]
+			levelSlice = append(levelSlice, node.Val)
+			q = q[1:]
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+
+		result = append(result, levelSlice)
+	}
+
+	return result
+}
+
+func main() {
+	root := TreeNode{3,nil,nil}
+	root.Left = &TreeNode{9,nil,nil}
+	root.Right = &TreeNode{20,nil,nil}
+	root.Right.Left = &TreeNode{15,nil,nil}
+	root.Right.Right = &TreeNode{7,nil,nil}
+
+	result := LevelOrder(&root)
+
+	for _, levelSlice := range result {
+		for _, el := range levelSlice {
+			fmt.Printf("%d ", el)
+		}
+		fmt.Println("")
+	}
+}
