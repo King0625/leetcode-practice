f64497b2e883ed913cf52450577de196cb0975f8
kenchen <independentchen@gmail.com>
2025-06-07 02:03:54 +0800

    Feat: finish #71 nicely

---
+++ golang/0001-0100/0071.SimplifyPath/solution.go
@@ -0,0 +1,31 @@
+package main
+
+import "strings"
+
+func simplifyPath(path string) string {
+    basePath := "/"
+
+	locations := strings.Split(path, "/")
+
+	var stack []string
+	var stackSize int
+
+	for _, location := range locations {
+		if location == "" || location == "." {
+			continue
+		} else if location == ".." {
+			stackSize = len(stack)
+			if stackSize == 0 {
+				continue
+			}
+			stack = stack[:stackSize-1]
+		} else {
+			stack = append(stack, location)
+		}
+	}
+
+	result := strings.Join(stack, "/")
+
+	return basePath + result
+}
+
