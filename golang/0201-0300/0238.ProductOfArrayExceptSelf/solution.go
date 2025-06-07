1b2d1d5fbfcd63bae24ba6dc4b7bdd8ba5ea31a3
kenchen <independentchen@gmail.com>
2025-06-07 20:04:08 +0800

    Feat: finish #238 nicely

---
+++ golang/0201-0300/0238.ProductOfArrayExceptSelf/solution.go
@@ -0,0 +1,27 @@
+package leetcode
+
+func productExceptSelf(nums []int) []int {
+  	var zeroCount int
+
+	productWithoutZero := 1
+	for _, num := range nums {
+		if num == 0 {
+			zeroCount++
+		} else {
+			productWithoutZero *= num
+		}
+	}
+
+	result := make([]int, len(nums))
+
+	for i, num := range nums {
+		if num != 0 && zeroCount == 0 {
+			result[i] = productWithoutZero / num
+		} else if num == 0 && zeroCount == 1 {
+			result[i] = productWithoutZero
+		}
+	}
+
+	return result
+}
+
