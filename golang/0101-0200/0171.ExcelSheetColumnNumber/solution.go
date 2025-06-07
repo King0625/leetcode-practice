5910136536d1ecce26434ce40ecdec51c7106bf8
kenchen <independentchen@gmail.com>
2025-06-07 13:30:59 +0800

    Feat: finish #171 nicely

---
+++ golang/0101-0200/0171.ExcelSheetColumnNumber/solution.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import "math"
+
+func titleToNumber(columnTitle string) int {
+	result := 0
+
+	power := len(columnTitle) - 1
+	for _, letter := range(columnTitle) {
+		result += (int(letter - 'A') + 1) * int(math.Pow(26.0, float64(power)))
+		power--
+	}
+
+	return result
+}
+
