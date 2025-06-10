fd89e265f9b7253f1a32aa5edec8e1eff4c42668
kenchen <independentchen@gmail.com>
2025-06-10 23:08:16 +0800

    Feat: finish #61 but there may be possibilities to refactor

---
+++ golang/0001-0100/0061.RotateList/solution.go
@@ -0,0 +1,50 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	var listLen int
+
+	dummy := head
+
+	for dummy != nil {
+		listLen++
+		dummy = dummy.Next
+	}
+
+	dummy = head
+
+	realK := k % listLen
+	
+	if realK == 0 {
+		return head
+	}
+
+	for listLen-1 > realK {
+		dummy = dummy.Next
+		listLen--
+	}
+
+	rotated := dummy.Next
+
+	dummy.Next = nil
+
+	dummy = rotated
+
+	for realK > 1 {
+		dummy = dummy.Next
+		realK--
+	}
+
+	dummy.Next = head
+
+	return rotated
+}
+
