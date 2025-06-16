7892fb9327c0ea5c1929ff7ca6c8537f6ae622e6
kenchen <independentchen@gmail.com>
2025-06-17 06:59:10 +0800

    Feat: finish #2016 nicely

---
+++ golang/2001-2100/2016.MaximumDifferenceBetweenIncreasingElements/solution.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func maximumDifference(nums []int) int {
+	result := -1
+	
+	minNum := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < minNum {
+			minNum = nums[i]
+		}
+
+		if nums[i] > minNum && nums[i]-minNum > result {
+			result = nums[i]-minNum
+		}
+
+	}
+
+	return result
+}
+
