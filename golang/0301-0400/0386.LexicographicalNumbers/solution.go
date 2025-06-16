d4eaa22b129eb99d2a00ba329dd29798f6f359a9
kenchen <independentchen@gmail.com>
2025-06-17 07:14:56 +0800

    Feat: finish #386 with lookups. Need to study some approaches about lexicographical problems

---
+++ golang/0301-0400/0386.LexicographicalNumbers/solution.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func lexicalOrder(n int) []int {
+	var result []int
+	current := 1
+
+	for i := 0; i < n; i++ {
+		result = append(result, current)
+
+		if current*10 <= n {
+			current *= 10
+		} else {
+			for current%10 == 9 || current >= n {
+				current /= 10
+			}
+			current++
+		}
+	}
+
+	return result
+}
+
+func main() {
+	n := 113
+	result := lexicalOrder(n)
+	fmt.Println(result)
+}
