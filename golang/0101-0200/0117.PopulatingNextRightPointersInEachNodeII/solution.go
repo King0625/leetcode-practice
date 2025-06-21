8d6e9ab4e2b4731def0775cd097f841bbaf09d2d
kenchen <independentchen@gmail.com>
2025-06-21 09:50:58 +0800

    Feat: finish #117 normally

---
+++ golang/0101-0200/0117.PopulatingNextRightPointersInEachNodeII/solution.go
@@ -0,0 +1,39 @@
+package leetcode
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+
+	var queue []*Node
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		size := len(queue)
+		for range size {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		for i := 0; i < len(queue)-1; i++ {
+			queue[i].Next = queue[i+1]
+		}
+
+	}
+
+	return root
+}
