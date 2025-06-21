74c9cb63a7cb3087c5967faad42e9fdddeb268c7
kenchen <independentchen@gmail.com>
2025-06-21 14:58:05 +0800

    Feat: don't access value from map again (did improve some memory)

--- golang/0001-0100/0001.TwoSum/solution.go
+++ golang/0001-0100/0001.TwoSum/solution.go
@@ -3,16 +3,15 @@ package leetcode
 func twoSum(nums []int, target int) []int {
 	subtractMap := make(map[int]int)
 
-	var result []int
-
 	for i, num := range nums {
-		if _, ok := subtractMap[num]; ok {
-			result = append(result, subtractMap[num], i)
+        diff := target-num
+		if val, ok := subtractMap[num]; ok {
+            return []int{val, i}
 		} else {
-			subtractMap[target-num] = i
+			subtractMap[diff] = i
 		}
 	}
 
-	return result
+	return nil
 }
 
