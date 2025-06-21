5e5c200978f55f9d8da6764415e037295cdc6469
kenchen <independentchen@gmail.com>
2025-06-21 15:56:19 +0800

    Feat: finish #9 and meet the follow up

---
+++ golang/0001-0100/0009.PalindromeNumber/solution.go
@@ -0,0 +1,26 @@
+package leetcode
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	var digits []int
+	
+	for x  > 0 {
+		digits = append(digits, x%10)
+		x /= 10
+	}
+
+	left, right := 0, len(digits)-1
+
+	for left < right {
+		if digits[left] != digits[right] {
+			return false
+		}
+		left++
+		right--
+	}
+
+	return true
+}
+
