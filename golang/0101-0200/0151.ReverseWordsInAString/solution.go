352bcf3eff40d83cb6bd2813ce21053527a85c02
kenchen <independentchen@gmail.com>
2025-06-21 14:52:58 +0800

    Refactor: use Reverse function in slices package instead of manually swaps

--- golang/0101-0200/0151.ReverseWordsInAString/solution.go
+++ golang/0101-0200/0151.ReverseWordsInAString/solution.go
@@ -1,16 +1,13 @@
 package leetcode
 
-import "strings"
+import (
+	"slices"
+	"strings"
+)
 
 func reverseWords(s string) string {
 	wordSlice := strings.Fields(s)
-	
-	for i, j := 0, len(wordSlice)-1; i < j; i, j = i+1, j-1 {
-		wordSlice[i], wordSlice[j] = wordSlice[j], wordSlice[i]
-	}
-
-	result := strings.Join(wordSlice, " ")
-
-	return result
+	slices.Reverse(wordSlice)	
+	return strings.Join(wordSlice, " ")
 }
 
