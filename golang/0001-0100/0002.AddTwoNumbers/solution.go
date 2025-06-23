49dae83bd8210e731ef2cdab0b881e3792996d3e
kenchen <independentchen@gmail.com>
2025-06-23 23:51:37 +0800

    Feat: Improve both time and memory in #2. No need to store all node values in slices at first.

--- golang/0001-0100/0002.AddTwoNumbers/solution.go
+++ golang/0001-0100/0002.AddTwoNumbers/solution.go
@@ -6,55 +6,43 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var digits1, digits2 []int
-
-	for l1 != nil {
-		digits1 = append(digits1, l1.Val)
-		l1 = l1.Next
-	}
-
-	for l2 != nil {
-		digits2 = append(digits2, l2.Val)
-		l2 = l2.Next
-	}
-
-	var i1, i2 int
 	var hasCarry bool
-	result := &ListNode{Val: 0}
-	dummy := result
+	var val1, val2 int
+	newList := &ListNode{0, nil}
+	dummy := newList
 
-	for i1 < len(digits1) || i2 < len(digits2) {
-		var newNum int
+	for l1 != nil || l2 != nil {
+		if l1 != nil {
+			val1 = l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			val2 = l2.Val
+			l2 = l2.Next
+		}
+
+		result := val1 + val2
 		if hasCarry {
-			newNum++
+			result += 1
 		}
 
-		if i2 >= len(digits2) {
-			newNum += digits1[i1]
-			i1++
-		} else if i1 >= len(digits1) {
-			newNum += digits2[i2]
-			i2++
-		} else {
-			newNum += digits1[i1] + digits2[i2]
-			i1++
-			i2++
-		}
-
-		if newNum/10 > 0 {
+		if result >= 10 {
+			result %= 10
 			hasCarry = true
-			newNum %= 10
 		} else {
 			hasCarry = false
 		}
 
-		dummy.Next = &ListNode{Val: newNum}
+		dummy.Next = &ListNode{result, nil}
+		val1 = 0
+		val2 = 0
 		dummy = dummy.Next
 	}
 
 	if hasCarry {
-		dummy.Next = &ListNode{Val: 1}
+		dummy.Next = &ListNode{1, nil}
 	}
 
-	return result.Next
+	return newList.Next
 }
