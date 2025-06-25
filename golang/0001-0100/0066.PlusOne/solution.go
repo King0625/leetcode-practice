7bad9fa5d4d29c108480e126ef1394faa757a0f1
kenchen <independentchen@gmail.com>
2025-06-25 22:55:50 +0800

    Feat: finish #66 nicely

---
+++ golang/0001-0100/0066.PlusOne/solution.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func plusOne(digits []int) []int {
+	idx := len(digits)-1
+
+	for idx >= 0 {
+		addByOne := digits[idx]+1
+		if addByOne == 10 {
+			digits[idx] = 0
+		} else {
+			digits[idx]++
+			break
+		}
+
+		idx--
+	}
+
+	if idx < 0 {
+		digits = append([]int{1}, digits...)
+	}
+
+	return digits
+}
+
