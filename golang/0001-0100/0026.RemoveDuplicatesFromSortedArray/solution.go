4f8447c2e1e3283a7a1477212c820f74dcb5ae2b
kenchen <independentchen@gmail.com>
2025-06-25 19:51:00 +0800

    Feat: finish #26 with lookups. Review how to delete array elements in place

---
+++ golang/0001-0100/0026.RemoveDuplicatesFromSortedArray/solution.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	idx := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[idx] = nums[i]
+			idx++
+		}
+	}
+
+	return idx
+}
+
