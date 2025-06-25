f0e06e81277bd14e8ddb920c86307bd46b94598b
kenchen <independentchen@gmail.com>
2025-06-25 23:09:06 +0800

    Feat: use byte type and no need to check length for stack pop. Greatly Improved time and memory

--- golang/2301-2400/2390.RemovingStarsFromAString/solution.go
+++ golang/2301-2400/2390.RemovingStarsFromAString/solution.go
@@ -1,19 +1,16 @@
 package leetcode
 
 func removeStars(s string) string {
-	var runes []rune
+	var bytes []byte
 
-	for _, r := range s {
-		if r == '*' {
-			currentLen := len(runes)
-			if currentLen > 0 {
-				runes = runes[:currentLen-1]
-			}
-		} else {
-			runes = append(runes, r)
-		}
-	}
+    for i := 0; i < len(s); i++ {
+        if s[i] == byte('*') {
+            bytes = bytes[:len(bytes)-1]
+        } else {
+            bytes = append(bytes, s[i])
+        }
+    }
 
-	return string(runes)
+	return string(bytes)
 }
 
