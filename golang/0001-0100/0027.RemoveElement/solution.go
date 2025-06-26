9ca55cb930653f50fccaaa872620202df2b26817
kenchen <independentchen@gmail.com>
2025-06-26 14:47:42 +0800

    Feat: finish #27 nicely

---
+++ golang/0001-0100/0027.RemoveElement/solution.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func removeElement(nums []int, val int) int {
+	idx := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[idx] = nums[i]
+			idx++
+		}
+	}
+
+	fmt.Println(nums)
+	return idx
+}
+
