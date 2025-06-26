8f44731c1bd05455eb704addc577c8df88eae084
kenchen <independentchen@gmail.com>
2025-06-26 15:01:45 +0800

    Feat: finish #73 nicely, but there's an O(1) memory follow-up

---
+++ golang/0001-0100/0073.SetMatrixZeroes/solution.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func setZeroes(matrix [][]int)  {
+	rows, cols := len(matrix), len(matrix[0])
+	var rowToZero, colToZero []int
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if matrix[i][j] == 0 {
+				rowToZero = append(rowToZero, i)
+				colToZero = append(colToZero, j)
+			}
+		}
+	}
+
+	for _, x := range rowToZero {
+		for i := 0; i < cols; i++ {
+			matrix[x][i] = 0
+		}
+	}
+
+	for _, y := range colToZero {
+		for j := 0; j < rows; j++ {
+			matrix[j][y] = 0
+		}
+	}
+}
+
+func main() {
+	matrix := [][]int{
+		{0,1,2,0},{3,4,5,2},{1,3,1,5},
+	}
+	setZeroes(matrix)
+	fmt.Println(matrix)
+}
