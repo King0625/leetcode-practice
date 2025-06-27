f7d3b16912e8cc1df997dcee614d048f26e6afeb
kenchen <independentchen@gmail.com>
2025-06-27 16:36:38 +0800

    Feat: finish #35 nicely. Have to practice more binary search problems

---
+++ golang/0001-0100/0035.SearchInsertPosition/solution.go
@@ -0,0 +1,27 @@
+package leetcode
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	result := -1
+
+	for left <= right {
+		mid := (right + left) / 2
+
+		if nums[mid] == target {
+			result = mid
+			break
+		} else if nums[mid] > target {
+			right = mid-1
+		} else {
+			left = mid+1
+		}
+	}
+
+	
+	if result == -1 {
+		result = left
+	}
+
+	return result
+}
+
