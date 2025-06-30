6be7a2e7eeb3d7447ee8d25459904264e176c961
kenchen <independentchen@gmail.com>
2025-06-30 15:55:55 +0800

    Feat: finish #114 with lookups. Maybe need to understand other solutions

---
+++ golang/0101-0200/0114.FlattenBinaryTreeToLinkedList/solution.go
@@ -0,0 +1,25 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func flatten(root *TreeNode) {
+	var prevNode *TreeNode
+
+	var preorder func(*TreeNode)
+	flat = func(node *TreeNode) {
+		if node != nil {
+			flat(node.Right)
+			flat(node.Left)
+
+			node.Right = prevNode
+			node.Left = nil
+			prevNode = node
+		}
+	}
+
+	flat(root)
+}
