ae485e7669a8e61b20e674f6e6fd782d53bc9793
kenchen <independentchen@gmail.com>
2025-07-01 23:06:50 +0800

    Feat: finish 3330 with bad time and memory

---
+++ golang/3301-3400/3330.FindTheOriginalTypedStringI/solution.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func possibleStringCount(word string) int {
+	var letterMap [26]int
+
+	lastLetterAt := -1
+	for _, r := range word {
+		at := int(r - 'a')
+		if lastLetterAt == at {
+			letterMap[at]++
+		}
+
+		lastLetterAt = at
+	}
+
+	result := 1
+	for _, count := range letterMap {
+		result += count-1
+	}
+
+	return result
+}
+
+func main() {
+	word := "ere"
+	fmt.Println(possibleStringCount(word))
+}
