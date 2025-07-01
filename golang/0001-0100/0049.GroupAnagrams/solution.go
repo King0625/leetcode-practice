d9c8a977c52041c8acd1de204fb05c4047a46e10
kenchen <independentchen@gmail.com>
2025-07-01 22:35:17 +0800

    Feat: store index into groups map. Slightly improve time and space

--- golang/0001-0100/0049.GroupAnagrams/solution.go
+++ golang/0001-0100/0049.GroupAnagrams/solution.go
@@ -3,30 +3,34 @@ package leetcode
 import "slices"
 
 func groupAnagrams(strs []string) [][]string {
-	groups := make(map[string][]string)
+	groups := make(map[string][]int)
 
-	for _, str := range strs {
+	for i, str := range strs {
 		var runes []rune
 
 		for _, r := range str {
 			runes = append(runes, r)
 		}
-		
+
 		slices.Sort(runes)
 
 		sortedStr := string(runes)
 
 		if _, ok := groups[sortedStr]; !ok {
-			groups[sortedStr] = []string{str}
+			groups[sortedStr] = []int{i}
 		} else {
-			groups[sortedStr] = append(groups[sortedStr], str)
+			groups[sortedStr] = append(groups[sortedStr], i)
 		}
 	}
 
 	var result [][]string
 
 	for _, v := range groups {
-		result = append(result, v)
+        var group []string
+        for _, idx := range v {
+            group = append(group, strs[idx])
+        }
+		result = append(result, group)
 	}
 
 	return result
