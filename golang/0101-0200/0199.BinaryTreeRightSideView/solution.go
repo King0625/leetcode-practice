55f24126130236e1c29adb9f0b4165275e5f1f6e
kenchen <independentchen@gmail.com>
2025-07-02 14:33:12 +0800

    Feat: finish #199 nicely

---
+++ golang/0101-0200/0199.BinaryTreeRightSideView/solution.go
@@ -0,0 +1,38 @@
+package leetcode
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func rightSideView(root *TreeNode) []int {
+	var result []int
+	if root == nil {
+		return result
+	}
+
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		qSize := len(queue)
+		for i := 0; i < qSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i == qSize - 1 {
+				result = append(result, node.Val)
+			}
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return result
+}
+
