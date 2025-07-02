7df6da690bd617e7004bf3f694b833730e22e9bf
kenchen <independentchen@gmail.com>
2025-07-02 17:41:53 +0800

    Feat: finish #84 with lookups and bad time and memory. Need to practice more about stack

---
+++ golang/0001-0100/0084.LargestRectangleInHistogram/solution.go
@@ -0,0 +1,31 @@
+package leetcode
+
+type Rectangle struct {
+	index int
+	height int
+}
+
+func largestRectangleArea(heights []int) int {
+	var result int
+	var stack []Rectangle
+
+	for i, height := range heights {
+		prevIndex := i
+		for len(stack) > 0 && stack[len(stack)-1].height > height {
+			calcHeight := stack[len(stack)-1].height * (i - stack[len(stack)-1].index)
+			result = max(result, calcHeight)
+			prevIndex = stack[len(stack)-1].index
+
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, Rectangle{prevIndex, height})
+	}
+
+	for _, val := range stack {
+		area := val.height * (len(heights)-val.index)
+		result = max(result, area)
+	}
+
+	return result
+}
+
