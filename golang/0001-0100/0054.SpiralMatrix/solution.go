b459d1f0069249d809c6d30ecd77dc934f640d71
kenchen <independentchen@gmail.com>
2025-07-02 15:27:21 +0800

    Feat: finish #54 nicely

---
+++ golang/0001-0100/0054.SpiralMatrix/solution.go
@@ -0,0 +1,49 @@
+package leetcode
+
+func spiralOrder(matrix [][]int) []int {
+	row := len(matrix)
+	col := len(matrix[0])
+
+	var result []int
+
+	rounds := 0
+	count := 0
+	total := row*col
+	for {
+		if count < total {
+			for i := rounds; i < col-rounds; i++ {
+				result = append(result, matrix[rounds][i])
+				count++
+			}
+		}
+
+		if count < total {
+			for j := rounds + 1; j < row-rounds; j++ {
+				result = append(result, matrix[j][col-rounds-1])
+				count++
+			}
+		}
+
+		if count < total {
+			for k := col - rounds - 2; k >= rounds; k-- {
+				result = append(result, matrix[row-rounds-1][k])
+				count++
+			}
+		}
+
+		if count < total {
+			for m := row - rounds - 2; m >= rounds+1; m-- {
+				result = append(result, matrix[m][rounds])
+				count++
+			}
+		}
+
+		if count == row*col {
+			break
+		}
+		rounds++
+	}
+
+	return result
+}
+
