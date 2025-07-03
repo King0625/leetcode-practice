d471f5b114c2d8cdb170f5f0e48b90c5fee58aab
kenchen <independentchen@gmail.com>
2025-07-03 15:43:01 +0800

    Feat: finish #230 with bad time

---
+++ golang/0201-0300/0230.KthSmallestElementinABST/solution.go
@@ -0,0 +1,24 @@
+package leetcode
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+	var nums []int
+	var inorder func(*TreeNode)
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		inorder(root.Left)
+		nums = append(nums, root.Val)
+		inorder(root.Right)
+	}
+
+	inorder(root)
+	return nums[k-1]
+}
