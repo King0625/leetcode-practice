ee4458ffc957a40d506e5a886bc942ebe8d770e8
kenchen <independentchen@gmail.com>
2025-07-04 20:39:46 +0800

    Feat: lookup one of the solutions: use dp to prevent solving the same subproblem. No tle occurs

--- golang/0301-0400/0329.LongestIncreasingPathInAMatrix/solution.go
+++ golang/0301-0400/0329.LongestIncreasingPathInAMatrix/solution.go
@@ -1,39 +1,47 @@
 package leetcode
 
-func longestIncreasingPath(matrix [][]int) int {
-	row, col := len(matrix), len(matrix[0])
-	result := 0
+var directions [4][2]int = [4][2]int{
+	{1, 0}, {-1, 0}, {0, 1}, {0, -1},
+}
 
-	var dfs func(int, int, int, int)
-	dfs = func(x, y, prev, count int) {
-		if x > row-1 || x < 0 || y > col-1 || y < 0 {
-			result = max(result, count)
-			return
-		}
-		if matrix[x][y] == -1 {
-			result = max(result, count)
-			return
-		}
-		if matrix[x][y] <= prev {
-			result = max(result, count)
-			return
-		}
-
-		count++
-		temp := matrix[x][y]
-		matrix[x][y] = -1
-		dfs(x+1, y, temp, count)
-		dfs(x-1, y, temp, count)
-		dfs(x, y-1, temp, count)
-		dfs(x, y+1, temp, count)
-		matrix[x][y] = temp
+func dfs(matrix [][]int, row, col, x, y int, dp [][]int) int {
+	if dp[x][y] != 0 {
+		return dp[x][y]
 	}
 
+	maxVal := 1
+
+	for i := 0; i < 4; i++ {
+		newX := x + directions[i][0]
+		newY := y + directions[i][1]
+
+		if newX < 0 || newY < 0 || newX >= row || newY >= col || matrix[newX][newY] <= matrix[x][y] {
+			continue
+		}
+
+		length := 1 + dfs(matrix, row, col, newX, newY, dp)
+		maxVal = max(maxVal, length)
+	}
+
+	dp[x][y] = maxVal
+	return maxVal
+
+}
+func longestIncreasingPath(matrix [][]int) int {
+	row, col := len(matrix), len(matrix[0])
+
+	dp := make([][]int, row)
+	for i := 0; i < row; i++ {
+		dp[i] = make([]int, col)
+	}
+	result := 1
 	for i := 0; i < row; i++ {
 		for j := 0; j < col; j++ {
-			dfs(i, j, -2, 0)
+			length := dfs(matrix, row, col, i, j, dp)
+			result = max(result, length)
 		}
 	}
 
 	return result
 }
+
