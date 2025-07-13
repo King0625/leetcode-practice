b599fe8b3c3f6ad5429b2ca5e41d6d462389bcb0
kenchen <independentchen@gmail.com>
2025-07-13 20:50:21 +0800

    Feat: finish #2081 with lookups. Need to see other solutions and revisit it several times

--- golang/2001-2100/2081.SumOfK-MirrorNumbers/solution.go
+++ golang/2001-2100/2081.SumOfK-MirrorNumbers/solution.go
@@ -1,74 +1,52 @@
-package main
+package leetcode
 
-import "fmt"
-import "strconv"
+func isPalindrome(x int64, k int, digit []int) bool {
+	length := -1
 
-func generateTenPalindromes(from int) []string {
-	var palindromes []string
-
-	// i is the "half" we mirror
-	for i := from; len(palindromes) < 10; i++ {
-		s := strconv.Itoa(i)
-
-		// Odd-length palindrome: e.g. i=12 → "12" + "1" = "121"
-		odd := s + reverseString(s[:len(s)-1])
-		palindromes = append(palindromes, odd)
-
-		// Even-length palindrome: e.g. i=12 → "12" + "21" = "1221"
-		even := s + reverseString(s)
-		palindromes = append(palindromes, even)
-
-		if len(palindromes) >= count {
-			break
-		}
-
+	for x > 0 {
+		length++
+		digit[length] = int(x % int64(k))
+		x /= int64(k)
 	}
 
-	return palindromes
-}
-
-func reverseString(s string) string {
-	runes := []rune(s)
-	n := len(runes)
-	for i := 0; i < n/2; i++ {
-		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
-	}
-	return string(runes)
-}
-
-func isPalindrome(numStr string) bool {
-	left, right := 0, len(numStr)-1
-
-	for left < right {
-		if numStr[left] != numStr[right] {
+	for i, j := 0, length; i < j; i, j = i+1, j-1 {
+		if digit[i] != digit[j] {
 			return false
 		}
-		left++
-		right--
 	}
 
 	return true
 }
 
 func kMirror(k int, n int) int64 {
-	var result int64
-	var currentNum int64 = 1
-	for n > 0 {
-		decimalNumStr := strconv.FormatInt(currentNum, 10)
-		basedNumStr := strconv.FormatInt(currentNum, k)
+	digit := make([]int, 100)
+	left, count, ans := 1, 0, int64(0)
+	for count < n {
+		right := left * 10
+		// op % 2 means odd or even length
+		for op := 0; op < 2; op++ {
+			for i := left; i < right && count < n; i++ {
+				combined := int64(i)
+				x := i
+				if op == 0 {
+					x = i / 10
+				}
 
-		if isPalindrome(decimalNumStr) && isPalindrome(basedNumStr) {
-			fmt.Printf("%s ", decimalNumStr)
-			result += currentNum
-			n--
+				for x > 0 {
+					combined = combined*10 + int64(x%10)
+					x /= 10
+				}
+
+				if isPalindrome(combined, k, digit) {
+					count++
+					ans += combined
+				}
+
+			}
 		}
-		currentNum++
+
+		left = right
 	}
 
-	return result
-}
-
-func main() {
-	k, n := 7, 17
-	fmt.Println(kMirror(k, n))
+	return ans
 }
